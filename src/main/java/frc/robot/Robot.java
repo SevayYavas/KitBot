f715e4249fb058f6735b393243034b0aa91f5461
SevayYavas <yavassevay6@gmail.com>
2025-01-11 18:36:52 +0300

    Umarim olmuştur

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,7 +5,14 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import 
+import com.revrobotics.CANSparkMax;
+import edu.wpi.first.wpilibj.PS5Controller;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.Timer;
+
+
+
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -14,27 +21,62 @@ import
  * project.
  */
 public class Robot extends TimedRobot {
+  CANSparkMax frontleftmotor = new CANSparkMax(7,MotorType.kBrushed);
+  CANSparkMax backleftmotor = new CANSparkMax(6,MotorType.kBrushed);
+  CANSparkMax frontrightmotor = new CANSparkMax(9,MotorType.kBrushed);
+  CANSparkMax backrightmotor = new CANSparkMax(8,MotorType.kBrushed);
+
+  CANSparkMax roller = new CANSparkMax(10, MotorType.kBrushed);
+
+  Timer timer = new Timer();
+
+  PS5Controller ps5joystick = new PS5Controller(0);
+  DifferentialDrive m_robotdrive = new DifferentialDrive(frontleftmotor, frontrightmotor);
+
+
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
    */
   @Override
-  public void robotInit() {}
+  public void robotInit() {
+    backrightmotor.setInverted(true);
+    frontrightmotor.setInverted(true);
+    backleftmotor.follow(frontleftmotor);
+    backrightmotor.follow(frontleftmotor);
+  }
 
   @Override
   public void robotPeriodic() {}
 
   @Override
-  public void autonomousInit() {}
+  public void autonomousInit() {
+    timer.reset();
+    timer.start();
+  }
 
   @Override
-  public void autonomousPeriodic() {}
+  public void autonomousPeriodic() {
+    if(timer.get() < 10.0){
+      m_robotdrive.arcadeDrive(0.5, 0.5);
+    }
+    else if(timer.get() > 10.0){
+      m_robotdrive.arcadeDrive(0.0, 0.0);
+    }
+    
+  }
 
   @Override
-  public void teleopInit() {}
+  public void teleopInit() {
+    m_robotdrive.arcadeDrive(ps5joystick.getRightX(), ps5joystick.getRightY());
+  }
 
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    if(ps5joystick.getRawButton(1)){
+      roller.set(0.5);
+    }
+  }
 
   @Override
   public void disabledInit() {}
